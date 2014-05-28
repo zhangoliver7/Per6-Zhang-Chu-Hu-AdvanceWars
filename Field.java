b0e5f5a37a36ab161738577cd8dad8a12fbc1d8c
Oliver Zhang <zhangoliver7@gmail.com>
2014-05-27 21:47:47 -0400

    Attempting ToString of Field

--- Field.java
+++ Field.java
@@ -1,12 +1,25 @@
 public class Field {
-    private Terrain[][] field;
+    private String[][] field;
     
-    /*public Field() {
-	field = new Terrain[15][15];
+    public Field() {
+	field = new String[15][15];
     }
 
     public Field(int x, int y) {
-	field 
-	}*/
+	field = new String[x][y]; 
+    }
+
+    public static void main(String[] args) {
+	File file = new File("Field1.txt");
+	
+	try {
+	    Scanner sc = new Scanner(File);
+	    while (sc.hasNextLine()) {
+		String s = sc.next();
+				
+	    }
+	    
+	}
+    }
 
 }
