932f2fa53e003967a77ad027ad45f479fcadf06c
Alex <ylsdhu@hotmail.com>
2014-06-04 21:48:01 -0400

    Added some parameters for movement, will write that code soon. Adjusting Field.java will be necessary.

--- Terrain.java
+++ Terrain.java
@@ -1,16 +1,33 @@
 public enum Terrain{
 
-    plain(1,1);
+    plain(1,1,"plain");
 
     private final int def;
     private final int movefoot;
     private final String name;
-
+    private Terrain prevTerrain;
+    private boolean selected;
+    private int xcor;
+    private int ycor;
 
     Terrain(int def, int movefoot, String name){
 	this.def = def;
 	this.movefoot = movefoot;
 	this.name = name;
+	selected = false;
+    }
+
+    public int getXcor(){
+	return xcor;
+    }
+
+    public int getYcor(){
+	return ycor;
+    }
+
+    public int[] getCor(){
+	int[] result = {xcor,ycor};
+	return result;
     }
 
     public int getDef(){
@@ -19,6 +36,22 @@ public enum Terrain{
 
     public int getMovefoot(){
 	return movefoot;
+    }
+
+    public void toggleSelect(){
+	selected = !(selected);
+    }
+
+    public void clearSelect(){
+	selected = false;
+    }
+
+    public void clearHistory(){
+	prevTerrain = null;
+    }
+
+    public void setprevTerrain(Terrain prev){
+	prevTerrain = prev;
     }
 
 }
